7e1b301bd285d13ee0296731ee18ed41499b11be
Walaa VOILIDAS <walaavolidis@gmail.com>
2024-10-27 15:01:43 +0200

    create increment and decrement for counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+"use client";
+import React, { useState } from 'react';
 
 export default function Home() {
-  return (    
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrement = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
     <div>
-         <h1>Walaa is Cool!</h1>
-    </div>   
+      <h1>{counter}</h1>
+      <div>
+      <button onClick={handleIncrement}>Increment</button>
+      </div>
+      <div>
+      <button onClick={handleDecrement}>Decrement</button>
+      </div>
+    </div>
   );
 }
