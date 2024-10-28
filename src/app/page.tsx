4bc108581dcb8567d499700b841a7e3a154185b8
Walaa VOILIDAS <walaavolidis@gmail.com>
2024-10-28 12:11:56 +0200

    using inline functions for increment and decrement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,14 @@ import React, { useState } from 'react';
 export default function Home() {
   const [counter, setCounter] = useState(0);
 
-  const handleIncrement = () => {
-    setCounter(prev => prev + 1);
-  };
-
-  const handleDecrement = () => {
-    setCounter(prev => prev - 1);
-  };
-
   return (
     <div>
       <h1>{counter}</h1>
       <div>
-      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={()=> setCounter(prev => prev + 1)}>Increment</button>
       </div>
       <div>
-      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={()=> setCounter(prev => prev - 1)}>Decrement</button>
       </div>
     </div>
   );
