789390c9eacd22fcd282e163351272a65fbdc232
Walaa VOILIDAS <walaavolidis@gmail.com>
2024-10-29 13:56:43 +0200

    use localstorage for counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
   const [counter, setCounter] = useState(0);
-
+  useEffect(() => {
+    const storedCounter = localStorage.getItem("counter");
+    if (storedCounter) {
+      setCounter(parseInt(storedCounter));
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("counter", counter.toString());
+  }, [counter]);
   return (
     <div>
       <h1>{counter}</h1>
