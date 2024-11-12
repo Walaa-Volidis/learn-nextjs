12dc66511c9c86b25689e741b9237189a449f0ff
Walaa VOILIDAS <walaavolidis@gmail.com>
2024-11-12 08:28:14 +0200

    remove try and catch

--- src/app/api/deleteTask/[id]/route.ts
+++ src/app/api/deleteTask/[id]/route.ts
@@ -5,18 +5,11 @@ export async function DELETE(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  try {
-    await prisma.task.delete({
-      where: {
-        id: parseInt(params.id, 10),
-      },
-    });
+  await prisma.task.delete({
+    where: {
+      id: parseInt(params.id, 10),
+    },
+  });
 
-    return NextResponse.json({ message: "Task successfully deleted" });
-  } catch (error) {
-    return NextResponse.json(
-      { error: (error as Error).message },
-      { status: 400 }
-    );
-  }
+  return NextResponse.json({ message: "Task successfully deleted" });
 }
