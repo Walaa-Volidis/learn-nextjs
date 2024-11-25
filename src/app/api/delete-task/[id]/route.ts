a1ebabf9af8585379b61bd1af8ee078dc4130c76
Walaa VOILIDAS <walaavolidis@gmail.com>
2024-11-25 14:10:18 +0200

    comment delete route

--- src/app/api/delete-task/[id]/route.ts
+++ src/app/api/delete-task/[id]/route.ts
@@ -1,23 +1,24 @@
-import prisma from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+// import prisma from "@/lib/prisma";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  try {
-    const { id } = params;
-    const deletedTask = await prisma.task.delete({
-      where: {
-        id: parseInt(id, 10),
-      },
-    });
-    return NextResponse.json(deletedTask, { status: 200 });
-  } catch (error) {
-    console.error("Error deleting task:", error);
-    return NextResponse.json(
-      { error: "An error occurred while deleting the task" },
-      { status: 400 }
-    );
-  }
-}
+// export async function DELETE(
+//   request: NextRequest,
+//   context: { params: Promise<{ id: string }> } // Wrap params in a Promise
+// ) {
+//   try {
+//     const { id } = await context.params; // Await the params if it's a Promise
+//     const deletedTask = await prisma.task.delete({
+//       where: {
+//         id: parseInt(id, 10),
+//       },
+//     });
+
+//     return NextResponse.json(deletedTask, { status: 200 });
+//   } catch (error) {
+//     console.error("Error deleting task:", error);
+//     return NextResponse.json(
+//       { error: "An error occurred while deleting the task" },
+//       { status: 400 }
+//     );
+//   }
+// }
