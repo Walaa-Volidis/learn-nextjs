6f2e7154e63d4b1a961e6b573415067d994b95db
Walaa VOILIDAS <walaavolidis@gmail.com>
2024-11-27 13:35:59 +0200

    remove ws

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,7 @@
-import ws from "ws";
 import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { Pool, neonConfig } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
 const prismaClientSingleton = () => {
-  neonConfig.webSocketConstructor = ws;
   const connectionString = `${process.env.DATABASE_URL}`;
   const pool = new Pool({ connectionString });
   const adapter = new PrismaNeon(pool);
